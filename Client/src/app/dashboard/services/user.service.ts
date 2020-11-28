abd7e0257d5fca90692aa702354f022e01b6b120
Rafał Huk <r.huk@outlook.com>
2020-11-28 20:03:22 +0100

    UserService

---
+++ Client/src/app/dashboard/services/user.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  baseUrl: string = 'https://localhost:5001/User'
+  public currentUser: Observable<User>
+  
+  constructor(private http: HttpClient) { }
+
+  getVeterans() {
+    return this.http.get(this.baseUrl + '/GetAllVeterans')
+  }
+}
